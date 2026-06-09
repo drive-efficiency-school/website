b29cd56251a8e6f309c355e0974891295a3f64cf
Dhamodharan Krishnan <dhamukrish@gmail.com>
2026-06-09 22:23:18 +0530

    chore(version): build stamp bump for prod deploy (v1.2.4 26060901)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
 
   const emit = defineEmits(['navigate'])
 
-  const version = 'v1.2.3 (26060702)'
+  const version = 'v1.2.4 (26060901)'
   const copied = ref(false)
   const currentYear = new Date().getFullYear()
 
